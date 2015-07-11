5a69ceef7baa2b10ad3d7e8ac81f938b0caaddb2
Alban Diquet <nabla.c0d3@gmail.com>
2015-07-10 22:37:19 -0700

    Initial commit

---
+++ SSLKillSwitch/SSLKillSwitch.h
@@ -0,0 +1,19 @@
+//
+//  SSLKillSwitch.h
+//  SSLKillSwitch
+//
+//  Created by Alban Diquet on 7/10/15.
+//  Copyright (c) 2015 Alban Diquet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SSLKillSwitch.
+FOUNDATION_EXPORT double SSLKillSwitchVersionNumber;
+
+//! Project version string for SSLKillSwitch.
+FOUNDATION_EXPORT const unsigned char SSLKillSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSLKillSwitch/PublicHeader.h>
+
+
---
+++ SSLKillSwitch/SSLKillSwitch.m
@@ -0,0 +1,129 @@
+//
+//  SSLKillSwitch.m
+//  SSLKillSwitch
+//
+//  Created by Alban Diquet on 7/10/15.
+//  Copyright (c) 2015 Alban Diquet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Security/SecureTransport.h>
+//#import "substrate.h"
+
+#define PREFERENCEFILE "/private/var/mobile/Library/Preferences/com.nablac0d3.SSLKillSwitchSettings.plist"
+
+
+static void MSHookFunction(void *test, void *test2, void **test3)
+{
+    // TBD
+}
+
+
+#pragma mark Utility Functions
+
+void SSKLog(NSString *format, ...)
+{
+    // Only log in debug builds
+#if DEBUG
+    NSString *newFormat = [[NSString alloc] initWithFormat:@"=== SSL Kill Switch 2: %@", format];
+    va_list args;
+    va_start(args, format);
+    NSLogv(newFormat, args);
+    va_end(args);
+#endif
+}
+
+
+// Utility function to read the Tweak's preferences
+static BOOL shouldHookFromPreference(NSString *preferenceSetting)
+{
+    BOOL shouldHook = NO;
+    NSMutableDictionary* plist = [[NSMutableDictionary alloc] initWithContentsOfFile:@PREFERENCEFILE];
+    
+    if (!plist)
+    {
+        SSKLog(@"Preference file not found.");
+    }
+    else
+    {
+        shouldHook = [[plist objectForKey:preferenceSetting] boolValue];
+        SSKLog(@"Preference set to %d.", shouldHook);
+    }
+    return shouldHook;
+}
+
+
+#pragma mark SSLSetSessionOption Hook
+
+static OSStatus (*original_SSLSetSessionOption)(SSLContextRef context,
+                                                SSLSessionOption option,
+                                                Boolean value);
+
+static OSStatus replaced_SSLSetSessionOption(SSLContextRef context,
+                                             SSLSessionOption option,
+                                             Boolean value)
+{
+    // Remove the ability to modify the value of the kSSLSessionOptionBreakOnServerAuth option
+    if (option == kSSLSessionOptionBreakOnServerAuth)
+    {
+        return noErr;
+    }
+    return original_SSLSetSessionOption(context, option, value);
+}
+
+
+#pragma mark SSLCreateContext Hook
+
+static SSLContextRef (*original_SSLCreateContext)(CFAllocatorRef alloc,
+                                                  SSLProtocolSide protocolSide,
+                                                  SSLConnectionType connectionType);
+
+static SSLContextRef replaced_SSLCreateContext(CFAllocatorRef alloc,
+                                               SSLProtocolSide protocolSide,
+                                               SSLConnectionType connectionType)
+{
+    SSLContextRef sslContext = original_SSLCreateContext(alloc, protocolSide, connectionType);
+    
+    // Immediately set the kSSLSessionOptionBreakOnServerAuth option in order to disable cert validation
+    original_SSLSetSessionOption(sslContext, kSSLSessionOptionBreakOnServerAuth, true);
+    return sslContext;
+}
+
+
+#pragma mark SSLHandshake Hook
+
+static OSStatus (*original_SSLHandshake)(SSLContextRef context);
+
+static OSStatus replaced_SSLHandshake(SSLContextRef context)
+{
+    OSStatus result = original_SSLHandshake(context);
+    
+    // Hijack the flow when breaking on server authentication
+    if (result == errSSLServerAuthCompleted)
+    {
+        // Do not check the cert and call SSLHandshake() again
+        return original_SSLHandshake(context);
+    }
+    
+    return result;
+}
+
+
+#pragma mark Dylib Constructor
+
+__attribute__((constructor)) static void init(int argc, const char **argv)
+{
+    // Should we enable the hook ?
+    if (shouldHookFromPreference(@"killSwitchSSLHandshake"))
+    {
+        SSKLog(@"Hook Enabled.");
+        MSHookFunction((void *) SSLHandshake,(void *)  replaced_SSLHandshake, (void **) &original_SSLHandshake);
+        MSHookFunction((void *) SSLSetSessionOption,(void *)  replaced_SSLSetSessionOption, (void **) &original_SSLSetSessionOption);
+        MSHookFunction((void *) SSLCreateContext,(void *)  replaced_SSLCreateContext, (void **) &original_SSLCreateContext);
+    }
+    else
+    {
+        SSKLog(@"Hook Disabled.");
+    }
+}
+
